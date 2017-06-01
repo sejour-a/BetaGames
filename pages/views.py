6c4b018f47cf14dcec655987babe0dff4c8aaf1d
sejour-a <sejour_a@jormungandr.eu>
2017-06-01 13:30:11 +0200

    Lang change test?

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
 from django.template import loader
+from django.http import HttpResponse
 
 
 def index(request):
     template = loader.get_template("index.html")
-    return HttpResponse(template.render())
+    context = {}
+    return HttpResponse(template.render(context, request))
